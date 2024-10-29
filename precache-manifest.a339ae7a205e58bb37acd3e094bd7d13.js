d642a13ec7a10a7f4f73be7c06f1e8825229352c
SrinjayMitra <srinjaymitra918@gmail.com>
2024-10-29 03:17:03 -0700

    Updates

---
+++ precache-manifest.a339ae7a205e58bb37acd3e094bd7d13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "932bbf6c5f0f763f6e8cc1c1533441e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5982d34947dda9b77212",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "aea54c2cc0e9daa36ce6",
+    "url": "/static/js/2.bbc244fd.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "5982d34947dda9b77212",
+    "url": "/static/js/main.c52684ed.chunk.js"
+  },
+  {
+    "revision": "cd2988c65fcca3ce3217",
+    "url": "/static/js/runtime~main.b45b9353.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
