dfe7a9ec4112dd22cd01a1b747df7eda78477d81
SrinjayMitra <srinjaymitra918@gmail.com>
2024-10-29 07:04:04 -0700

    added the interests section

---
+++ src/components/Interests.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Interests.css';
+
+const Interests = () => {
+  return (
+    <section className="interests">
+      <h3>Interests</h3>
+      <ul>
+        <li>Hobby 1</li>
+        <li>Hobby 2</li>
+        {/* Add more interests as needed */}
+      </ul>
+    </section>
+  );
+};
+
+export default Interests;
