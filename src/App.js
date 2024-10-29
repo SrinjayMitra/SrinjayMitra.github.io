7d7d5d764ee180b9df630e0d663f2b92a5e624cd
SrinjayMitra <srinjaymitra918@gmail.com>
2024-10-29 06:56:40 -0700

    chnages to app.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
-// import Experience from './components/Experience';
-// import Education from './components/Education';
+import Education from './components/Education';
 // import Interests from './components/Interests';
 // import Contact from './components/Contact';
 // import Footer from './components/Footer';
@@ -17,9 +16,8 @@ const App = () => {
       <Hero />
       <Projects />
       <Skills />
-     {/* <Experience />
       <Education />
-      <Interests />
+     {/* <Interests />
       <Contact />
       <Footer /> */}
     </div>
