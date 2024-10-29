52b5fd186c070826795f96f3ebe76a2c9beec0b5
SrinjayMitra <srinjaymitra918@gmail.com>
2024-10-29 06:38:57 -0700

    added the skills section

---
+++ src/components/Skills.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Skills.css';
+
+const Skills = () => {
+  return (
+    <section className="skills">
+      <h3>Skills</h3>
+      <ul>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Node.js</li>
+        {/* Add more skills as needed */}
+      </ul>
+    </section>
+  );
+};
+
+export default Skills;
