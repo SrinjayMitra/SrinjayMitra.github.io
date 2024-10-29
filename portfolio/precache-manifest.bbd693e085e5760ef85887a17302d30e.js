eb2232a9ee7c42959fbc6c8dd1e43d8951dfbc85
SrinjayMitra <srinjaymitra918@gmail.com>
2024-10-29 06:50:35 -0700

    Deploy React app to /portfolio from main branch

---
+++ portfolio/precache-manifest.bbd693e085e5760ef85887a17302d30e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dc9a1a74eea49d483cfb04c5fce649a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "51b6173002b5eae2b747",
+    "url": "/portfolio/static/css/main.4641ab25.chunk.css"
+  },
+  {
+    "revision": "aea54c2cc0e9daa36ce6",
+    "url": "/portfolio/static/js/2.bbc244fd.chunk.js"
+  },
+  {
+    "revision": "0eb3ca9d0061837f6c8f",
+    "url": "/portfolio/static/js/3.31c88699.chunk.js"
+  },
+  {
+    "revision": "51b6173002b5eae2b747",
+    "url": "/portfolio/static/js/main.f1f2fbe4.chunk.js"
+  },
+  {
+    "revision": "2e27f67191283bf89140",
+    "url": "/portfolio/static/js/runtime~main.20578482.js"
+  },
+  {
+    "revision": "1843a02febf15b912937a7754d72f623",
+    "url": "/portfolio/static/media/pic.1843a02f.jpg"
+  }
+]);
--- portfolio/service-worker.js
+++ portfolio/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f0fee34453c632fe50d41525772bc176.js"
+  "/portfolio/precache-manifest.bbd693e085e5760ef85887a17302d30e.js"
 );
 
 self.addEventListener('message', (event) => {
