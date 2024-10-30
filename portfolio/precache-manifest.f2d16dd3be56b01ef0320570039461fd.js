49f199d77997d98afdd77937277ea17e60101905
SrinjayMitra <srinjaymitra918@gmail.com>
2024-10-29 20:47:58 -0700

    added the links to personal infos

---
+++ portfolio/precache-manifest.f2d16dd3be56b01ef0320570039461fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0be20efbcff881590fe3e1fc00d51862",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a27fd814b51ed2de4461",
+    "url": "/portfolio/static/css/main.d6625d57.chunk.css"
+  },
+  {
+    "revision": "6634aa4ea9dd93bd8424",
+    "url": "/portfolio/static/js/2.cd9d04ee.chunk.js"
+  },
+  {
+    "revision": "5099652deffd979d970e",
+    "url": "/portfolio/static/js/3.d5f86460.chunk.js"
+  },
+  {
+    "revision": "a27fd814b51ed2de4461",
+    "url": "/portfolio/static/js/main.b810b5be.chunk.js"
+  },
+  {
+    "revision": "8b8dc6394bb8e24e9ae4",
+    "url": "/portfolio/static/js/runtime~main.ca896061.js"
+  },
+  {
+    "revision": "1843a02febf15b912937a7754d72f623",
+    "url": "/portfolio/static/media/pic.1843a02f.jpg"
+  }
+]);
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,18 +3,18 @@ import './Contact.css';
 
 const Contact = () => {
   return (
-    <section className="contact">
+    <section  id="contact" className="contact">
       <h3>Contact</h3>
       <p>Feel free to connect with me!</p>
       <ul>
         <li>
-          <a href="https://www.linkedin.com/in/your-profile" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+          <a href="https://www.linkedin.com/in/srinjay-mitra" target="_blank" rel="noopener noreferrer">LinkedIn</a>
         </li>
         <li>
           <a href="https://github.com/SrinjayMitra" target="_blank" rel="noopener noreferrer">GitHub</a>
         </li>
         <li>
-          <a href="/path/to/your/resume.pdf" target="_blank" rel="noopener noreferrer">Resume/CV</a>
+          <a href="https://filebin.net/nofs4nvpdmba20yt" target="_blank" rel="noopener noreferrer">Resume/CV</a>
         </li>
       </ul>
     </section>
