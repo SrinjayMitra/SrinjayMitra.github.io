1a7fd0923a6c8b03714cbdde73cf7989412f3030
SrinjayMitra <srinjaymitra918@gmail.com>
2024-10-29 20:41:14 -0700

    Fix the backgorund color of Skills and Interests

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,23 +3,25 @@ import './Skills.css';
 
 const Skills = () => {
   const skills = [
-    'JavaScript',
-    'React',
-    'Node.js',
-    'CSS',
-    'HTML',
-    'Python',
-    'java',
-    'c++'
-    // Add more skills as needed
+    { name: 'JavaScript', logo: 'https://img.icons8.com/color/48/000000/javascript.png' },
+    { name: 'React', logo: 'https://img.icons8.com/color/48/000000/react-native.png' },
+    { name: 'Node.js', logo: 'https://img.icons8.com/color/48/000000/nodejs.png' },
+    { name: 'CSS', logo: 'https://img.icons8.com/color/48/000000/css3.png' },
+    { name: 'HTML', logo: 'https://img.icons8.com/color/48/000000/html-5.png' },
+    { name: 'Python', logo: 'https://img.icons8.com/color/48/000000/python.png' },
+    { name: 'Java', logo: 'https://img.icons8.com/color/48/000000/java-coffee-cup-logo.png' },
+    { name: 'C++', logo: 'https://img.icons8.com/color/48/000000/c-plus-plus-logo.png' }
   ];
 
   return (
-    <section className="skills">
+    <section id="skills" className="skills">
       <h3>Skills</h3>
       <ul>
         {skills.map((skill, index) => (
-          <li key={index}>{skill}</li>
+          <li key={index} className="skill-item">
+            <img src={skill.logo} alt={`${skill.name} logo`} className="skill-logo" />
+            <span>{skill.name}</span>
+          </li>
         ))}
       </ul>
     </section>
